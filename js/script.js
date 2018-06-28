fa261261b679972d6cf8c1e1ef464cb97e8b1340
qrames <ramque@orange.fr>
2018-06-28 11:40:14 +0200

    dual work on circlebars

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log('>>>script.js');
+// <div class="input-group mb-3">
+//     <input type="text" class="form-control" placeholder="Nouvelle t&acirc;che" aria-label="task" aria-describedby="basic-addon2">
+//     <div class="input-group-append">
+//         <button class="btn btn-outline-secondary" type="button">Ajouter votre T&acirc;che</button>
+//     </div>
+// </div>
+
+$(document).ready(function($) {
+    //timer POMODORO
+    function time(){
+        $("#timer").value
+    }
+    new Circlebar({
+        element : "#circle-1"
+    });
+    $("#circle-1").Circlebar({
+        skin: "yellowcircle",
+        maxValue: 25,
+        fontSize: "14px",
+        triggerPercentage: true
+    });
+});
