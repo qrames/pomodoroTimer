5e7a12c78a01b340f099a7358a205e077a1b9a71
qrames <ramque@orange.fr>
2018-06-28 15:02:38 +0200

    Task and this Task work

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
 console.log('>>>script.js');
-// <div class="input-group mb-3">
-//     <input type="text" class="form-control" placeholder="Nouvelle t&acirc;che" aria-label="task" aria-describedby="basic-addon2">
-//     <div class="input-group-append">
-//         <button class="btn btn-outline-secondary" type="button">Ajouter votre T&acirc;che</button>
-//     </div>
-// </div>
 
 $(document).ready(function($) {
     //timer POMODORO
     new Circlebar({
-        element : "#timer",
-        skin : "yellow",
-        size : "200px",
+        element: "#timer",
+        skin: "yellow",
+        size: "200px",
+    });
+    // $(".input-group").each(function(index, el) {
+    //     Sortable.create(el);
+    // });
+    var toDo = document.getElementById("toDo");
+    $(toDo).css("height", "100%")
+    Sortable.create(toDo, {
+        group: "toDo"
+    });
+
+    function newTask(text) {
+        var task = $('<div>');
+        task.addClass("input-group input-group-sm mb-3");
+        var textTask = $('<p>');
+        textTask.addClass("form-control").text(text);
+        var trash = $('<div class="input-group-append"><button class="deleteTask btn btn-outline-secondary" type="button"><i class="far fa-trash-alt"></i></button></div>');
+        task.append(textTask);
+        task.append(trash);
+
+        $(toDo).append(task);
+        initTaskFunctions();
+    }
+    function initTaskFunctions(){
+        $(".deleteTask").click(function() {
+            console.log($(this).parent());
+            $(this).parent().parent().remove();
+        });
+
+    }
+
+    $("#addTask").click(function() {
+        newTask($("#newTask").val());
+        $("#newTask").val('');
     });
 });
